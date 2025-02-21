266b3bb59bf705c4e3e74ad6f3032b9c2ab473f2
Marijn van Aalst <NA>
2025-02-21 10:50:09 +0100

    add stg_events model

---
+++ models/stg_events.sql
@@ -0,0 +1,19 @@
+WITH stg_events AS (
+    SELECT * FROM {{ source('raw_data', 'events') }}
+)
+
+SELECT 
+    group_id,
+    name,
+    description,
+    TIMESTAMP_MILLIS(created) AS meetup_created,
+    TIMESTAMP_MILLIS(CAST(time AS INT64)) AS meetup_time,
+    SAFE_DIVIDE(duration, 60000) AS duration,
+    rsvp_limit,
+    venue_id,
+    status,
+    user_id,
+    stg_events.when AS time_rsvpd,
+    response,
+    guests
+FROM stg_events
